cce10538169aeb71fa55a440507265ac33ac607c
Sylvain METAYER <s.metayer@groupeonepoint.com>
2023-11-03 14:50:44 +0100

    update qrcode

--- checklist.sh
+++ checklist.sh
@@ -9,3 +9,5 @@ sudo systemctl stop cbagentd.service
 sudo systemctl stop forticlient.service
 
 echo "TODO Generate QR Code"
+qrencode https://openfeedback.io/bdxio2023/2023-11-10/622 -o slides/assets/img/openfeedback.png
+qrencode https://r.sylvain.dev/bdxio-2023 -o slides/assets/img/slides_link.png
