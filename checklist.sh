879e05c1c6f1beef3d68db635d8c996b06b79210
Sylvain <git@sylvain.dev>
2023-11-10 11:46:23 +0100

    comment qrcode generation

--- checklist.sh
+++ checklist.sh
@@ -8,5 +8,5 @@ sudo systemctl stop docker.service
 sudo systemctl stop cbagentd.service
 sudo systemctl stop forticlient.service
 
-qrencode https://openfeedback.io/bdxio2023/2023-11-10/622 -o slides/assets/img/openfeedback.svg -t SVG -l H -m 2
-qrencode https://r.sylvain.dev/bdxio-2023 -o slides/assets/img/slides_link.svg -t SVG -l H -m 2
+#qrencode https://openfeedback.io/bdxio2023/2023-11-10/622 -o slides/assets/img/openfeedback.svg -t SVG -l H -m 2
+#qrencode https://r.sylvain.dev/bdxio-2023 -o slides/assets/img/slides_link.svg -t SVG -l H -m 2
